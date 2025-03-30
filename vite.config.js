d0580a03c28aa45782b7ac1a054a37604b990dfb
NTPV <iq36@yandex.ru>
2025-03-30 19:24:13 +0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/test10/', // Укажите имя вашего репозитория
   plugins: [vue()],
-  // Другие настройки, если необходимо
+  base: '/test10/',
 });
